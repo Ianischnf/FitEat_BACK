05672e33d41b99a03b4336c78da5ca407907f944
SESAME\i_chenn <ianis.chennaf@infotel.com>
2025-03-20 15:16:04 +0100

    verification connexion mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 
+console.log("Connexion à MONGO_URI : ", process.env.MONGO_URI);
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
